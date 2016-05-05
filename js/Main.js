c06a9155ddf194b971cfe106fc73c8048e4a3c21
logiclogue <jordanpaullord@hotmail.co.uk>
2016-05-05 18:45:44 +0100

    Fix last

--- js/Main.js
+++ js/Main.js
@@ -5,7 +5,6 @@ var Pattern = require('./Pattern');
  * Main class which is called upon page load.
  */
 var Main = function () {
-    var word = new Word();
     var pattern = new Pattern('CVC');
 
     pattern.generateWord();
