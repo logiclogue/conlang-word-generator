31528f141bc3b22fe9d56c62e08480640021512a
logiclogue <jordanpaullord@hotmail.co.uk>
2016-05-09 16:36:18 +0100

    Move input example text to separate file

---
+++ js/Examples.js
@@ -0,0 +1,30 @@
+module.exports = [
+`CVCE 10
+CV 10
+VCV 10
+VCE 10
+CVCVCE 10
+
+(C):
+p 10
+t 10
+k 10
+s 10
+m 10
+n 10
+l 10
+w 10
+j 10
+
+(V):
+a 21
+e 23
+i 34
+o 12
+u 23
+
+(E):
+upom 10
+ist 10
+ewn 10
+`];
--- js/Main.js
+++ js/Main.js
@@ -1,77 +1,17 @@
 var Word = require('./Word');
 var Elements = require('./Elements');
 var Interpreter = require('./Interpreter');
+var Examples = require('./Examples');
 
 
 /*
  * Main class which is called upon page load.
  */
 var Main = function () {
-    this.word = new Word({
-        patterns: [
-            ['CVC', 10],
-            ['CV', 10],
-            ['CVV', 10],
-            ['VCV', 10],
-            ['VC', 10],
-            ['CVCCVC', 10],
-            ['CVCVC', 10],
-            ['CVCCV', 10],
-            ['C', 10]
-        ],
-        sounds: {
-            'C': [
-                ['p', 100],
-                ['t', 100],
-                ['k', 100],
-                ['s', 100],
-                ['m', 100],
-                ['n', 100],
-                ['l', 100],
-                ['w', 100],
-                ['j', 100]
-            ],
-            'V': [
-                ['a', 21],
-                ['e', 23],
-                ['i', 34],
-                ['o', 12],
-                ['u', 23],
-                ['汉', 50]
-            ]
-        }
-    });
+    this.word = new Word();
     this.elements = new Elements();
     this.interpreter = new Interpreter('textarea-input');
-    this.input = `CVCE 10
-CV 10
-VCV 10
-VCE 10
-CVCVCE 10
-
-(C):
-p 10
-t 10
-k 10
-s 10
-m 10
-n 10
-l 10
-w 10
-j 10
-
-(V):
-a 21
-e 23
-i 34
-o 12
-u 23
-
-(E):
-upom 10
-ist 10
-ewn 10
-`;
+    this.input = Examples[0];
 
     this.elements.get('button-output').addEventListener('click', this.outputClick.bind(this));
     this.elements.get('textarea-input').value = this.input;
