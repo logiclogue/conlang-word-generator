baf5479a922e0e21df05e8fe62e0fe581b7db806
logiclogue <jordanpaullord@hotmail.co.uk>
2016-05-27 22:15:30 +0100

    Comments

--- js/InterpreterDOM.js
+++ js/InterpreterDOM.js
@@ -1,10 +1,15 @@
 var Interpreter = require('./Interpreter');
 
 
+/*
+ * Interpreter class which is used to interpret
+ * input from the DOM.
+ */
 var InterpreterDOM = function () {
     Interpreter.call(this);
 };
 
+// Extends Interpreter
 InterpreterDOM.prototype = new Interpreter();
 
 (function () {
